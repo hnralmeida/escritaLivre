fc7a2e5b9def20efcb54409edffdd8f50b77f307
hnralmeida <55263158+hnralmeida@users.noreply.github.com>
2020-07-27 21:12:47 -0300

    Update triagem.c

--- triagem.c
+++ triagem.c
@@ -2,22 +2,15 @@
 #include<stdlib.h>
 
 int main (){
-    float temperatura, i =0, pacientes = 2;
+    fflush(stdin);
+    float temperatura, i =0, pacientes = 3, gripario = 0, geral= 0;
     char tosse, febre, ar;
 
     for (i=0; i<pacientes; i++){
-        printf("\nO usuario deve respontes S (sim) ou N (não) para as seguintes perguntas:");
+        printf("\nO usuario deve respontes S (sim) ou N (nao) para as seguintes perguntas:");
 
-        printf("\nTosse? ");
-        scanf("%c", &tosse);
-        fflush(stdin);
-
-        printf("\nFebre? ");
-        scanf("%c", &febre);
-        fflush(stdin);
-
-        printf("\nFalta de Ar? ");
-        scanf("%c", &ar);
+        printf("\nTosse? Febre? Falta de Ar? \n");
+        scanf("%c %c %c", &tosse, &febre, &ar);
         fflush(stdin);
 
         printf("\nInsira a Temperatura: ");
@@ -25,12 +18,16 @@ int main (){
 
         if( (febre=='n' && ar == 'n' && temperatura < 37.5) ){
             printf("\nRecepcao Geral");
+            geral++;
         }
         if(febre=='s' || ar == 's' || temperatura > 37.5){
             printf("\nGripario");
+            gripario++;
         }
         printf("\n");
     }
+
+    printf("\nGripario = %.2f \nGeral=%.2f", (100*gripario/pacientes), (100*geral/pacientes) );
 
 return 0;
 }
