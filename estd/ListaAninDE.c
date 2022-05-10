52e13c4b57097169dcbf3cdce85d2914cdb52824
Henrique <55263158+hnralmeida@users.noreply.github.com>
2022-05-10 14:33:33 -0300

    Create ListaAninDE.c

---
+++ estd/ListaAninDE.c
@@ -0,0 +1,8 @@
+/*
+AUTORES: 	Henrique Almeida de Oliveira
+			Luiz Eduardo Marchiori
+Disciplina: Estrutura de Dados
+Professor: Vanderson José Idelfonso Silva
+
+Programa em linguagem C que implementa uma Lista aninhada formada em sua camada mais externa por uma lista duplamente encadeada de Cursos.
+*/
