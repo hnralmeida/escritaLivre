4c7b2118f71db8a69df978eed10e2d197bb765f3
Henrique <55263158+hnralmeida@users.noreply.github.com>
2022-05-25 14:34:52 -0300

    adicionar e listar 25/05

--- estd/ListaAninDE.c
+++ estd/ListaAninDE.c
@@ -1,9 +1,11 @@
 /*
+=========================================================================================
 AUTORES: 	Henrique Almeida de Oliveira
-		Luiz Eduardo Marchiori
+			Luiz Eduardo Marchiori
 Disciplina: Estrutura de Dados
 Professor: Vanderson José Idelfonso Silva
 Programa em linguagem C que implementa uma Lista aninhada formada em sua camada mais externa por uma lista duplamente encadeada de Cursos.
+=========================================================================================
 */
 
 #include<stdio.h>
@@ -11,6 +13,10 @@ Programa em linguagem C que implementa uma Lista aninhada formada em sua camada 
 #include<locale.h>
 #include<stdlib.h>
 
+/*
+=========================================================================================
+			Tipo Abstrato de Dados
+*/
 typedef char string[40];
 
 typedef struct Lista lista;
@@ -76,72 +82,35 @@ void excluirAluno();
 void listarAluno(lista* l);
 void adicionaDisciplina(lista *l, string nomeCurso, string nomeTurma, string nomeAluno, string nomeDisciplina);
 
+/*
+=========================================================================================
+			MAIN
+*/
 int main (){
 	lista l;
 	inicializaLista(&l);
+	setlocale(LC_ALL, "");
 	while(1){
 		menuPrincipal(&l);
 	}
 }
 
-int menu (lista *l){
-	int opcao;
-	do{
-		printf("------------------------\n");
-		printf("Bem vindo ao menu!\n");
-		printf("Selecione uma opção: \n");
-		printf("0 - Sair\n1 - Cursos\n2- Turmas\n3- Alunos\n");
-		scanf("%d", &opcao);
-	}while ((opcao < 0) || (opcao > 3));
-	return opcao;
-}
-
-int menuCurso (lista *l){
-	int opcao;
-	system("CLS");
-	do{
-		printf("------------------------\n");
-		printf("Bem vindo ao menu de Cursos!\n");
-		printf("Selecione uma opção: \n");
-		printf("0 - Ir para o menu principal\n1 - Inserir Curso\n2- Excluir Curso\n3- Lista todos os cursos\n");
-		scanf("%d", &opcao);
-	}while ((opcao < 0) || (opcao > 3));
-	switch(opcao){
-	   case 1: adicionaCurso(l, "SI"); break;
-	   case 2: excluirCurso(); break;
-	   case 3: listarCurso(l); break;
-	}
-	if (opcao ==0){
-		menuPrincipal(l);
-	}
-}
-
-int menuTurma(lista *l){
-	int opcao;
-	system("CLS");
-	do{
-		printf("------------------------\n");
-		printf("Bem vindo ao menu de Turma!\n");
-		printf("Selecione uma opção: \n");
-		printf("0 - Ir para o menu principal\n1 - Inserir Turma\n2- Excluir Turma\n3- Listar todas as turmas\n");
-		scanf("%d", &opcao);
-	}while ((opcao < 0) || (opcao > 3));
-	if (opcao ==0){
-		menuPrincipal(l);
-		}
-	switch(opcao){
-	   case 1: adicionaTurma(l, "SI", "2021"); break;
-	   case 2: excluirTurma(); break;
-	   case 3: listarTurma(l); break;
-		}
+/*
+=========================================================================================
+			IMPLEMENTAÇÃO DAS FUNÇÕES
+*/
+void limparBuffer(){
+    char c;
+    while((c= getchar()) != '\n' && c != EOF);
 }
 
 int menuAluno (lista *l){
 	int opcao;
-	system("CLS");
+	//system("CLS");
 	do{
-		printf("------------------------\n");
+		printf("============================\n");
 		printf("Bem vindo ao menu de Alunos!\n");
+		printf("============================\n");
 		printf("Selecione uma opção: \n");
 		printf("0 - Ir para o menu principal\n1 - Inserir Aluno\n2- Excluir Aluno\n3- Listar todos os alunos\n");
 		scanf("%d", &opcao);
@@ -156,18 +125,86 @@ int menuAluno (lista *l){
 	}
 }
 
+int menuTurma(lista *l){
+	int opcao;
+	//system("CLS");
+	do{
+		printf("============================\n");
+		printf("Bem vindo ao menu de Turma!\n");
+		printf("============================\n");
+		printf("Selecione uma opção: \n");
+		printf("0 - Ir para o menu principal\n1 - Inserir Turma\n2- Excluir Turma\n3- Listar todas as turmas\n");
+		scanf("%d", &opcao);
+	}while ((opcao < 0) || (opcao > 3));
+	if (opcao ==0){
+		menuPrincipal(l);
+		}
+	switch(opcao){
+	   case 1: adicionaTurma(l, "SI", "2021"); break;
+	   case 2: excluirTurma(); break;
+	   case 3: listarTurma(l); break;
+		}
+}
+
+int menuCurso (lista *l){
+	int opcao;
+	//system("CLS");
+	do{
+		printf("============================\n");
+		printf("Bem vindo ao menu de Cursos!\n");
+		printf("============================\n");
+		printf("Selecione uma opção: \n");
+		printf("0 - Ir para o menu principal\n1 - Inserir Curso\n2- Excluir Curso\n3- Lista todos os cursos\n");
+		scanf("%d", &opcao);
+	}while ((opcao < 0) || (opcao > 3));
+	switch(opcao){
+	   case 1: 
+	   		printf("\nQual curso vai adicionar?\n");
+	   		string curso;
+	   		limparBuffer();
+	   		scanf(" %s", curso);
+			adicionaCurso(l, curso); 
+			break;
+	   case 2: 
+	   		excluirCurso(); 
+			break;
+	   case 3: 
+	   		listarCurso(l); 
+			break;
+	}
+	if (opcao ==0){
+		menuPrincipal(l);
+	}
+}
+
+int menu (lista *l){
+	int opcao;
+	do{
+		printf("============================\n");
+		printf("Bem vindo ao menu!\n");
+		printf("============================\n");
+		printf("Selecione uma opção: \n");
+		printf("0 - Sair\n1 - Cursos\n2- Turmas\n3- Alunos\n");
+		scanf("%d", &opcao);
+	}while ((opcao < 0) || (opcao > 3));
+	return opcao;
+}
+
 int menuPrincipal (lista *l){
 	int op;
-	//system("CLS");
-		op = menu(l);
-		switch(op){
-		   case 1: menuCurso(l); break;
-		   case 2: menuTurma(l); break;
-		   case 3: menuAluno(l); break;
-			}
-		if (op == 0){
-			exit(0);
+	op = menu(l);
+	switch(op){
+	   case 1: menuCurso(l); break;
+	   case 2: menuTurma(l); break;
+	   case 3: menuAluno(l); break;
 		}
+	if (op == 0){
+		exit(0);
+	}
+	limparBuffer();
+	printf("\nPressione qualquer tecla para continuar. . .");
+	char c = getchar();
+    system("@cls||clear");
 }
 
 void inicializaLista(lista *l){
@@ -191,11 +228,27 @@ void adicionaCurso(lista *l, string nomeCurso){
     
 	if(l->ini==NULL){
 		l->ini = no;
+		l->fim = no;
 	}else{
-		TCurso *aux = no->prox;
-		while(aux->prox!=NULL&&(strcmp(nomeCurso, aux->nomeCurso)<0))
+		TCurso *aux = l->ini;
+		while(aux->prox!=NULL&&(strcmp(nomeCurso, aux->nomeCurso)>0))
             aux = aux->prox;
-		aux->prox = no;
+            
+		if(aux->prox == NULL){
+        	aux->prox = no;
+        	no->ante = aux;
+        	l->fim = no;
+		}else if(aux->ante == NULL){
+			no->prox = aux;
+			aux->ante = no;
+			l->ini = no;
+		}else{
+			TCurso* aux1 = aux->prox;
+			no->prox = aux1;
+			no->ante = aux;
+			aux->prox = no;
+			aux1->ante = no;
+		}
 	}
 }
 
@@ -206,11 +259,11 @@ void listarCurso(lista* l){
 	TCurso* aux;
 	aux = l->ini;
 	int i, max = l->total;
-	printf("============================\n");
-	printf("\tLista de Cursos");
-	printf("============================\n");
+	printf("\n============================");
+	printf("\n\tLista de Cursos");
+	printf("\n============================");
 	for (i=0; i<max; i++){
-		printf("- %s\n", aux->nomeCurso);
+		printf("\n- %s", aux->nomeCurso);
 		aux = aux->prox;
 	}
 }
@@ -228,13 +281,13 @@ void adicionaTurma(lista *l, string nomeCurso, string nomeTurma){
     inicializaTurma(no, nomeTurma);
     
 	if(l->ini==NULL){
-		printf("Lista Vazia");
+		printf("\nLista Vazia: crie um curso antes");
 		return;
 	}else{
 		TCurso *aux = l->ini;
 		while(aux!=NULL && strcmp(aux->nomeCurso, nomeCurso)<0) aux = aux->prox;
 		if(aux == NULL){
-			printf("Curso não encontrado");
+			printf("\nCurso não encontrado: adicione o curso antes");
 			return;
 		}
 		aux->total++;
@@ -243,7 +296,7 @@ void adicionaTurma(lista *l, string nomeCurso, string nomeTurma){
 			aux->turmas = no;
 		}else{
 			TTurma *aux1 = aux->turmas;
-			while(aux1->prox!=NULL&&(strcmp(nomeTurma, aux1->nomeTurma)<0))
+			while(aux1->prox!=NULL&&(strcmp(nomeTurma, aux1->nomeTurma)>0))
 	            aux1 = aux1->prox;
 	            
 	        if(aux1->prox == NULL){
@@ -267,20 +320,21 @@ void listarTurma(lista* l){
 	TCurso* aux;
 	aux = l->ini;
 	int i, max = l->total;
-	printf("============================\n");
-	printf("\tLista de Turmas");
-	printf("============================\n");
+	printf("\n============================");
+	printf("\n\tLista de Turmas");
+	printf("\n============================");
 	for (i=0; i<max; i++){
 		TTurma* aux1;
 		aux1 = aux->turmas;
 		
-		printf("- %s\n", aux->nomeCurso);
+		printf("\n- %s", aux->nomeCurso);
 		int j, max1 = aux->total;
 		
 		for(j=0; j<max1; j++){
-			printf("\t- %s\n", aux1->nomeTurma);
+			printf("\n\t- %s", aux1->nomeTurma);
 			aux1 = aux1->prox;
 		}
+		aux = aux->prox;
 	}
 }
 
@@ -298,27 +352,27 @@ void adicionaAluno(lista *l, string nomeCurso, string nomeTurma, string nomeAlun
     inicializaAluno(no, nomeAluno, gender);
     
 	if(l->ini==NULL){
-		printf("Lista Vazia");
+		printf("\nLista Vazia: crie um curso antes");
 		return;
 	}else{
 		TCurso *aux = l->ini;
 		while(aux!=NULL && strcmp(aux->nomeCurso, nomeCurso)) aux = aux->prox;
 		if(aux == NULL){
-			printf("Curso não encontrado");
+			printf("\nCurso não encontrado: adicione o curso antes");
 			return;
 		}
 		
 		TTurma *aux1 = aux->turmas;
-		while(aux1!=NULL && strcmp(aux1->nomeTurma, nomeTurma)<0) aux1 = aux1->prox;
+		while(aux1!=NULL && strcmp(aux1->nomeTurma, nomeTurma)) aux1 = aux1->prox;
 		if(aux1 == NULL){
-			printf("Turma não encontrada");
+			printf("\nTurma não encontrada: adicione a turma antes");
 			return;
 		}
 		aux1->total++;
 		
 		TAluno *aux2 = aux1->alunos;
 		if(aux2 == NULL){
-			aux2 = no;
+			aux1->alunos = no;
 		}else{
 			while(aux2->prox!=NULL&&(strcmp(nomeAluno, aux2->nome)<0))
 	            aux2 = aux2->prox;
@@ -344,19 +398,19 @@ void listarAluno(lista* l){
 	TCurso* aux;
 	aux = l->ini;
 	int i, max = l->total;
-	printf("============================\n");
-	printf("\tLista de Alunos");
-	printf("============================\n");
+	printf("\n============================");
+	printf("\n\tLista de Alunos");
+	printf("\n============================");
 	for (i=0; i<max; i++){
-		TTurma* aux1 = aux->turmas;
-		printf("- %s\n", aux->nomeCurso);		
+		TTurma* aux1 = aux->turmas; 
+		printf("\n-Curso: %s", aux->nomeCurso);		
 		int j, max1 = aux->total;
 		for(j=0; j<max1;j++){
-			printf("\t- %s\n", aux1->nomeTurma);
+			printf("\n\t-Turma: %s", aux1->nomeTurma);
 			TAluno* aux2 = aux1->alunos;
 			int k, max2 = aux1->total;
 			for(k=0; k<max2; k++){
-				printf("\t\tNome- %s\n", aux2->nome);
+				printf("\n\t\t-Nome: %s", aux2->nome);
 				aux2 = aux2->prox;
 			}
 			aux1 = aux1->prox;
