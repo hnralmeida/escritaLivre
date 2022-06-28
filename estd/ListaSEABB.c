ddc00cb21fb9c26c851fcbcc6d6de7ef88840808
henrique <almeida.hnr@gmail.com>
2022-06-27 22:49:10 -0300

    versao final apos correção

--- estd/ListaSEABB.c
+++ estd/ListaSEABB.c
@@ -6,7 +6,7 @@ Disciplina: Estrutura de Dados
 Professor: Vanderson José Idelfonso Silva
 Programa em linguagem C que implementa uma lista simplesmente encadeada com as seguintes informações: matricula, nome e sexo (além do campo prox, é claro).
 
-Uma árvore binária de busca (ABB) deverá permitir uma busca não linear por nome nessa mesma lista. Ou seja, qualquer procura por um elemento da lista com nome = "Jose da Silva", por exemplo, deverá começar pela árvore binária de busca. 
+Uma árvore binária de busca (ABB) deverá permitir uma busca não linear por nome nessa mesma lista. Ou seja, qualquer procura por um elemento da lista com nome = "Jose da Silva", por exemplo, deverá começar pela árvore binária de busca.
 Uma vez encontrado um nodo com esse nome, deve-se utilizar o campo enderecoLista (contendo um ponteiro para a Lista) para acessar o registro (struct) da Lista.
 
 Sempre que um elemento for inserido na Lista um nodo correspondente deverá ser inserido na árvore de modo a apontar (endereçar) o referido elemento.
@@ -42,6 +42,7 @@ typedef struct tipoPerson {
 typedef struct NoTree {
 	TTree *esq;
 	TTree *dir;
+	string nome;
 	TPerson *nodo;
 }TTree;
 
@@ -73,18 +74,18 @@ void automatico(TLista *L);
 int main(){
 	int op;
 	TLista lista;
-	setlocale(LC_ALL, "");
-	
+	setlocale(LC_ALL, "Portuguese");
+
 	inicializaLista(&lista);
-	
+
 	do {
 		op = menu();
-		
+
 		switch(op){
 		   case 1: inserePessoa(&lista); break;
 		   case 2: listaMatricula(&lista); break;
 		   case 3: listaNome(&lista); break;
-		   case 4: procuraNome(&lista); break;	
+		   case 4: procuraNome(&lista); break;
 		   case 5: removerLista(&lista); break;
 		   case 222: automatico(&lista); break;
 		}//switch
@@ -92,9 +93,9 @@ int main(){
 		limparBuffer();
 		char c = getchar();
 	    system("@cls||clear");
-		
+
 	} while(op != 0);
-	
+
 }
 
 /*
@@ -117,24 +118,24 @@ int menu(){
     printf("4 - Consultar Pessoa por Nome.\n");
     printf("5 - Remover pessoa.\n");
     printf("================================================\n");
-    
-    printf("\tInforme OPCAO desejada: ");
+
+    printf("\n\tInforme OPCAO desejada: ");
     scanf("%d",&opcao);
-    printf("================================================\n");
-    
+    printf("\n================================================\n");
+
     if (opcao == 222){
 		return opcao;
 	}
-	
+
 	if ((opcao > 5) || (opcao < 0)){
 		printf("\n\n\n");
-		printf("\t+------------------------+");
-		printf("\t|   ERRO:                |");
-		printf("\t|                        |");
-		printf("\t|   OPCAO INVALIDA!!!    |");
-		printf("\t|                        |");
-		printf("\t|   Tente outra vez.     |");
-		printf("\t+------------------------+");
+		printf("\n\t+------------------------+");
+		printf("\n\t|   ERRO:                |");
+		printf("\n\t|                        |");
+		printf("\n\t|   OPCAO INVALIDA!!!    |");
+		printf("\n\t|                        |");
+		printf("\n\t|   Tente outra vez.     |");
+		printf("\n\t+------------------------+");
 		system("PAUSE");
 	}
 	return opcao;
@@ -155,18 +156,19 @@ void inicializaPerson(TPerson* no, string nome, char gender, string reg){
 }
 
 // subfunção de inseraNaArvore, modifica os ponteiros
-TTree *inicializaTree(TPerson* add){
+TTree *inicializaTree(TPerson* add, string nome){
 	TTree *no = (TTree *)malloc(sizeof(TTree));
 	no->esq = NULL;
 	no->dir = NULL;
 	no->nodo = add;
+	strcpy(no->nome, nome);
 	return no;
 }
 
-// subfunção de inseraNaLista, 
+// subfunção de inseraNaLista,
 TTree *insereNaArvore(TTree **node, TPerson* add){
 	if((*node)==NULL){
-		(*node) = inicializaTree(add);
+		(*node) = inicializaTree(add, add->nome);
 	}else if(strcmp(add->nome, (*node)->nodo->nome)>0){
 		(*node)->dir = insereNaArvore(&(*node)->dir, add);
 	}else {
@@ -195,7 +197,7 @@ void insereNaLista(TLista *L, string nome, char genero, string matricula){
 			ant = aux;
 			aux = aux->prox;
 		}
-        
+
 		if(aux == NULL&&(ant!=L->ini)){
 			ant->prox = no;
 			L->fim = no;
@@ -214,19 +216,19 @@ void insereNaLista(TLista *L, string nome, char genero, string matricula){
 void inserePessoa(TLista *L){
 	string nome, matricula;
 	char genero;
-	
+
 	printf("\nDigite o nome do aluno: ");
 	limparBuffer();
 	scanf("%[^\n40]s", nome);
-	
+
 	printf("\nDigite o genero do aluno: ");
 	limparBuffer();
 	scanf("%c", &genero);
-	
+
 	printf("\nDigite o matricula do aluno: ");
 	limparBuffer();
 	scanf("%[^\n40]s", matricula);
-	
+
 	insereNaLista(L, nome, genero, matricula);
 }
 
@@ -268,7 +270,7 @@ void listaMatricula(TLista *L){
 // Subfunção de procuraNome, recursiva, retorna struct Person encontrada
 TPerson *digThrowTree(TTree* node, string name){
 	TPerson *x;
-	if(node!=NULL) x = node->nodo;	
+	if(node!=NULL) x = node->nodo;
 
 	if (node==NULL){
 		return NULL;
@@ -302,52 +304,105 @@ void procuraNome(TLista *L){
 	}
 }
 
-// Subfunção de removerLista, retira a pessoa da lista e seu respectivo nó na árvore
-TTree* removerGeral(TTree *st, string name){
-	//valor a ser comparado
-	TPerson *aux;
-	if(st!=NULL) aux = st->nodo;	
-
-	if (st==NULL){
-		//interrompe a recursão
-		return NULL;
-	}else if(strcmp(aux->nome, name)>0){
-		//valor procurado pra remover vem antes, desce pela direita
-		st->dir = removerGeral(st->dir, name);
-	}else if(strcmp(aux->nome, name)<0){
-		//valor procurado pra remover vem depois, desce pela esquerda
-		st->esq = removerGeral(st->esq, name);
-	}else{
-		//encontrou valor a remover, tratar quais ramos ele tem
-		if(st->dir==NULL && st->esq==NULL){
-			free(st);
-			st = NULL;
-		}else if (st->esq == NULL){
-			TTree* temp = st;
-			st= st->dir;
-			free(temp);
-		}else if (st->dir == NULL){
-			TTree* temp = st;
-			st= st->esq;
-			free(temp);
-		}else{
-			TTree* temp = st->esq;
-			while(temp->dir != NULL){
-				temp = temp->dir;
-			}
-			st->nodo = temp->nodo;
-			temp->nodo = aux;
-			st->esq = removerGeral(st->esq, name);
-		}
-		TPerson *faux;
-		faux = aux;
-		*aux = *(aux->prox);
-		free(faux);
+//subfuncao de removerGeral utilizada para pegar o nó mais a direita da subarvore esquerda do nó que será removido
+TTree *raizDireita(TTree **no){
+	if ((*no)->dir != NULL){
+		return raizDireita(&(*no)->dir);
+	} else {
+		TTree *aux1 = *no;
+		if ((*no)->esq != NULL)
+			*no = (*no)->esq;
+		else
+			*no = NULL;
+	return aux1;
 	}
-	return st;
 }
 
-// Remover Pessoa
+//subfuncao de removerLista, remove da árvore a pessoa
+void removerArvore(TLista *L, TTree **raiz, string apagar){
+	int resposta = 0;
+	if (*raiz == NULL){
+		printf("\nNome não encontrado!\n\n");
+		return;
+	}
+
+	if (strcmp(apagar, (*raiz)->nome) < 0){
+		removerArvore(L, &(*raiz)->esq, apagar);
+	} else if (strcmp(apagar, (*raiz)->nome) > 0){
+        removerArvore(L, &(*raiz)->dir, apagar);
+    } else {
+        printf("\n============================");
+        printf("\n   Apagar %s?\n   1 - Sim\n   0 - Nao\n\n-> ", (*raiz)->nome);
+        scanf("%i", &resposta);
+        printf("\n============================");
+        if (resposta){
+            TTree *aux = (*raiz);
+            if (((*raiz)->esq == NULL) && ((*raiz)->dir == NULL)){
+                free(aux);
+                (*raiz) = NULL;
+                if (L->raiz == (*raiz)) (L->raiz)=NULL;
+            } else if ((*raiz)->esq == NULL){
+                if (L->raiz == (*raiz)) (L->raiz)=(*raiz)->dir;
+                (*raiz) = (*raiz)->dir;
+                aux->dir = NULL;
+                free(aux);
+                aux = NULL;
+            }else if ((*raiz)->dir == NULL){
+                    if (L->raiz == (*raiz)) (L->raiz)=(*raiz)->esq;
+                    (*raiz) = (*raiz)->esq;
+                    aux->esq = NULL;
+                    free(aux);
+                    aux = NULL;
+            }else {
+                if (L->raiz == (*raiz)) (L->raiz)=aux;
+                aux = raizDireita(&(*raiz)->esq);
+                aux->esq = (*raiz)->esq;
+                aux->dir = (*raiz)->dir;
+                free((*raiz));
+                (*raiz) = aux;
+            }
+            printf("\n   %s apagado(a) !\n\n", apagar);
+        } else {
+            printf("\n============================");
+            printf("\n%s não apagado (a)", apagar);
+        }
+    }
+}
+
+void removerPessoa(TLista *l, string nome){
+    if(l->ini==NULL){
+		printf("\nLista Vazia: não há nada para excluir");
+		return;
+	}else{
+		TPerson *aux = l->ini;
+		TPerson *ante = NULL;
+		if(aux == NULL){
+			printf("\nNão há pessoas para remover na lista");
+		}else{
+			while(aux->prox!=NULL&&(strcmp(nome, aux->nome))){
+                ante = aux;
+                aux = aux->prox;
+			}
+
+			if(aux==NULL){
+				printf("\nPessoa não encontrado: revise informações antes de prosseguir");
+			}else if(aux->prox == l->fim){ //verificar se remover o ultimo
+				ante->prox = NULL;
+				l->fim = ante;
+				free(aux);
+			}else if(ante == NULL){ // verificar se remover o primeiro;
+				ante = aux->prox;
+				l->ini = ante;
+				free(aux);
+			}else{ // caso remover algum do meio qualquer
+				ante->prox = aux->prox;
+				free(aux);
+			}
+		}
+	}
+}
+
+// Remover Pessoa das estruturas
 void removerLista(TLista *L){
 
 	TTree *aux = L->raiz;
@@ -359,7 +414,8 @@ void removerLista(TLista *L){
 		string nome;
 		limparBuffer();
 		scanf("%[^\n40]s", nome);
-		aux = removerGeral(aux, nome);
+		removerArvore(L, &aux, nome);
+		removerPessoa(L, nome);
 		L->total--;
 	}
 }
@@ -367,11 +423,11 @@ void removerLista(TLista *L){
 void automatico(TLista *L){
 	string nome[10] = {"Yuri Duarte","Luiz Felipe Nascimento", "Calebe Fernandes" ,
 						"Maria Vitória Pires", "Diogo da Mota","Yasmin da Paz" ,
-						"Maria Julia Sales","Juliana Duarte", "Kaique da Rocha", 
+						"Maria Julia Sales","Juliana Duarte", "Kaique da Rocha",
 						"Lucas Gabriel Sales"};
-						
+
 	char genero[10] = "HHHMHMMMHH";
-	
+
 	string matricula[10] = {"0x01", "0x02", "0x03", "0x04", "0x05", "0x06", "0x07",
 							"0x08", "0x09", "0x0A"};
 	int i;
