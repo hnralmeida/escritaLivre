7488b87b2e1ed196afbe5467d435d2324772d016
Henrique <55263158+hnralmeida@users.noreply.github.com>
2022-08-11 13:43:52 -0300

    testing reading input

--- POO1/rapidTest.java
+++ POO1/rapidTest.java
@@ -8,42 +8,37 @@ import java.io.*;
 
 /**
  *
- * @hnralmeida
+ * @author hnralmeida
  */
 public class RapidProject {
-    
+
     /**
      * testing functions
      */
     public static void main(String[] args) {
-        System.out.println("Inicio");
-        
-        DataInputStream dado;
+        BufferedReader dado;
+        float a, n, media;
         String s = "";
-        float a,n;
-        try{
-            System.out.println("Entre com a nota 1");
-            dado = new DataInputStream(System.in);
-            s = dado.readLine( );
-            n = Float.parseFloat(s);
-            System.out.println("Entre com a nota 2");
-            dado = new DataInputStream(System.in);
-            s = dado.readLine();
-            a= Float.parseFloat(s);
         
-            if ( (n) >(a) ) {
-                System.out.println("Ayo n>a");
-            }else{
-                System.out.println("Hey a>n");
-            }
-        }
-        catch (IOException erro){
+        try {
+            System.out.println("Entre com a nota 1");
+            dado = new BufferedReader(new InputStreamReader(System.in));
+            s = dado.readLine();
+            n = Float.parseFloat(s);
+            
+            System.out.println("Entre com a nota 2");
+            dado = new BufferedReader(new InputStreamReader(System.in));
+            s = dado.readLine();
+            a = Float.parseFloat(s);
+            
+            media = (a + n) / 2;
+            System.out.println("Média: " + media);
+        } catch (IOException erro) {
             System.out.println("Erro na entrada de dados");
-        }
-        catch ( NumberFormatException erro ){
+        } catch (NumberFormatException erro) {
             System.out.println("Houve erro na conversão, digite apenas caracteres numericos");
         }
 
     }
-    
+
 }
