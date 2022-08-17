6faf21cca2bf4e353b786652906b1e3cb8e25bbc
Luiz Eduardo <102303587+luizeduardom@users.noreply.github.com>
2022-08-17 13:32:33 -0300

    :)

---
+++ TPA/exercicio3_Luiz/hash.h
@@ -0,0 +1,14 @@
+// -- Luiz Eduardo Marchiori --
+#ifndef HASH_H_
+#define HASH_H_
+
+// adiciona um vetor na hash
+void adicionarHash (Tlista lista[], unsigned long long int tam);
+
+// imprime valores na hash
+void printHash(Tlista lista[], unsigned long long int tam);
+
+// remove valores da hash
+removerHash(&tabelaHash, TAM); 
+
+#endif // HASH_H_
