21d43150ea2236ebc4c7aefbf8c1e0399ca794fe
Luiz Eduardo <102303587+luizeduardom@users.noreply.github.com>
2022-10-07 13:31:16 -0300

    Add files via upload

--- POO1/TheFinalQueue/src/thefinalqueue/Valkyrie.java
+++ POO1/TheFinalQueue/src/thefinalqueue/Valkyrie.java
@@ -5,6 +5,7 @@
  */
 package thefinalqueue;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -14,12 +15,28 @@ import java.util.LinkedList;
 public class Valkyrie extends Warrior {
     int id;
 
-    public Valkyrie(int tipo, int peso, int idade, String nome) {
-        super(tipo, peso, idade, nome);
+    public Valkyrie(int peso, int idade, String nome) {
+        super(peso, idade, nome);
     }
-
+    
+    /**
+     *
+     * @param energiaMaxima
+     */
+    
+    
+    @Override
+    public void setEnergiaMaxima(int energiaMaxima);
+    
+    
     @Override
     public void atacar(LinkedList timeA, LinkedList timeB) {
-        
+        Warrior warrior = (Warrior) timeB.get(0);
+        warrior.loseEnergy(20);
+        Iterator it = timeB.iterator();
+        if (it.hasNext()){
+            Warrior warrior2= (Warrior) timeB.get(1);
+            warrior2.setEnergiaAtual(warrior2.getEnergiaAtual() + 20);
+        }        
     }
 }
