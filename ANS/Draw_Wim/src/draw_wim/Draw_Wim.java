8b37b463ad446a192d2d6abc02771ff3f824e458
henrique <almeida.hnr@gmail.com>
2022-12-02 09:07:39 -0300

    telas 10/12

--- ANS/Draw_Wim/src/draw_wim/Draw_Wim.java
+++ ANS/Draw_Wim/src/draw_wim/Draw_Wim.java
@@ -12,8 +12,8 @@ public class Draw_Wim {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Draw Wimbledon");
-        frame.add(new IU00());
+        JFrame frame = new JFrame("Web Browser");
+        frame.add(new IU10());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
